aef78e348d13bd8ec1774fd40d7770d1f9c19661
Tarcizio Junior <62338122+JuniorTlitz@users.noreply.github.com>
2022-03-15 14:42:14 -0300

    test: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,18 +1,41 @@
 import { SignUpController } from './signup';
 // Classe que vai lidar com os error
 import { MissingParamsError } from '../errors/missing-params-error';
+import { InvalidParamsError } from '../errors/invalid-params-error';
+import { EmailValidator } from '../protocols/email-validator';
+
+interface SutTypes {
+  sut: SignUpController;
+  emailValidatorStub: EmailValidator;
+}
 
 /**
- * Movendo a criação do SUT para um factory
+ * Movendo a criação do SUT para um factory,
+ * ao adicionar dependencias ao nossos controllers, não vamos precisar fazer estanciar em todos os testes, apenas injetamos no MAKESUT
+ *
  */
-const makeSut = (): SignUpController => {
-  return new SignUpController();
+const makeSut = (): SutTypes => {
+  /**
+   * Não estamos preocupados como essa classe valida email, apenas queremos testar os retornos dessa função
+   * tomamos uma ação com o retorno da função
+   * Temos que garantir que o EmailValidatorStub implementa a interface EmailValidator
+   */
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub();
+  const sut = new SignUpController(emailValidatorStub);
+  return {
+    sut,
+    emailValidatorStub,
+  };
 };
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = makeSut();
-
+    const { sut } = makeSut();
     /**
      * Enviando os dados sem o name, sendo um campo obrigatorio!
      * Tem que dar um erro 400 quando não enviamos um request sem o campo NAME
@@ -31,8 +54,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Should return 400 if no email is provided', () => {
-    const sut = makeSut();
-
+    const { sut } = makeSut();
     /**
      * Enviando os dados sem o email, sendo um campo obrigatorio!
      * Tem que dar um erro 400 quando não enviamos um request sem o campo EMAIL
@@ -51,8 +73,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Should return 400 if no password is provided', () => {
-    const sut = makeSut();
-
+    const { sut } = makeSut();
     /**
      * Enviando os dados sem o password, sendo um campo obrigatorio!
      * Tem que dar um erro 400 quando não enviamos um request sem o campo PASSWORD
@@ -71,8 +92,7 @@ describe('SignUp Controller', () => {
   });
 
   test('Should return 400 if no password confirmation is provided', () => {
-    const sut = makeSut();
-
+    const { sut } = makeSut();
     /**
      * Enviando os dados sem o password confirmation, sendo um campo obrigatorio!
      * Tem que dar um erro 400 quando não enviamos um request sem o campo PASSWORD
@@ -93,4 +113,29 @@ describe('SignUp Controller', () => {
   });
 
   // ? Todos os campos foram validados
+
+  // todo --- Validar se o email é valido
+  test('Should return 400 if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut();
+    /**
+     * Mockando o valor do EmailValidatorStub de TRUE para FALSE
+     * Spy serve para simular a interação com o objeto, o mock era TRUE e com essa função ele passa a ter o valor como false
+     */
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+    /**
+     * Enviando um request com todos os parametros, e vamos validar se o EMAIL é valido
+     */
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@email.com', // não faz diferença o texto, apenas para dar mais semantica
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamsError('email'));
+  });
 });
