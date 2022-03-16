b965183648b8efdec92b9c936910b890439c0d52
Tarcizio Junior <62338122+JuniorTlitz@users.noreply.github.com>
2022-03-16 09:03:27 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,27 +4,35 @@ import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
 import { EmailValidator } from '../protocols/email-validator';
 import { InvalidParamsError } from '../errors/invalid-params-error';
+import { ServerError } from '../errors/server-error';
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
   constructor(emailValidator: EmailValidator) {
     this.emailValidator = emailValidator;
   }
   handle(httpRequest: httpRequest): httpResponse {
-    const requiredFields = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation',
-    ];
-    // Ao invez de fazer um IF para cada field, um loop para abrangir todo os fields
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamsError(field));
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
+      // Ao invez de fazer um IF para cada field, um loop para abrangir todo os fields
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamsError(field));
+        }
       }
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) {
-      return badRequest(new InvalidParamsError('email'));
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamsError('email'));
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
   }
 }
